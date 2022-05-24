8d93c08942850cb14c4c5e52901f80a0248db515
Yusuke <yusuke850208@gmail.com>
2022-05-24 22:08:24 +0200

    change favicons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Links from "./Component/Links"
+import LandingPage from "./Component/LandingPage";
+import Bio from "./Component/Bio";
+import Music from "./Component/Music";
+import Contact from "./Component/Contact"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
+      <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Links />
+      <Routes>
+      <Route exact path="/" element={<LandingPage />} />
+      <Route exact path="Bio" element={<Bio />} />
+      <Route exact path="Music" element={<Music />} />
+      <Route exact path="Contact" element={<Contact />} />
+      </Routes>
     </div>
+      </Router>
   );
 }
 
 export default App;
+     
+
