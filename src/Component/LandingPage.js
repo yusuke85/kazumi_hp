001ba2ad781cea0b32d25fa9fe9a6ebc9ffde9fc
Yusuke <yusuke850208@gmail.com>
2022-11-03 21:18:24 +0100

    repush

--- src/Component/LandingPage.js
+++ src/Component/LandingPage.js
@@ -33,7 +33,7 @@ const LandingPage = ({ playing, setPlaying }) => {
   return (
     <>
     <div class="sample01">
- <p>Next ※Barcelona※22/11- 26/11 [Instalación] Recorreguts Sonors 2022 @convento Sant Agustí</p> 
+ <p>Next ※Barcelona※22/11- 26/11[Instalación] Recorreguts Sonors 2022 @convento Sant Agustí</p> 
  </div>
  
       <h2 id="pleaseClick">Please click on the images!!</h2>
